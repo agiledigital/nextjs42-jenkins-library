f728a681bfd5a84b38f27b1d23961d24897bc2a6
Rory Stokes <contact@rorystok.es>
2018-11-16 11:03:46 +1100

    Update persistent volumes to include cache

--- vars/nextjs42BuilderPersistentVolumes.groovy
+++ vars/nextjs42BuilderPersistentVolumes.groovy
@@ -4,6 +4,11 @@ def call(Map config) {
       path: '/home/jenkins/.npm',
       claimName: "${config.project}-home-jenkins-npm",
       sizeGiB: 1
+    ],
+    [
+      path: '/home/jenkins/.cache',
+      claimName: "${config.project}-home-jenkins-cache",
+      sizeGiB: 2
     ]
   ]
 }
